33b09341e1d75a80605e0d5e788c2ff09abfb952
Harsh Valaki <96899883+TheLetterIsH@users.noreply.github.com>
2023-07-20 20:44:52 +0530

    🤸 Animations added!
    Added some animations to the player character.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+
+    private bool isWalking;
     
     private void Update()
     {
@@ -33,9 +35,16 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
 
+        isWalking = moveDir != Vector3.zero;
+
         float rotateSpeed = 15f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
 
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 
 }
